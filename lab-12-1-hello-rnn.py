2eb75c4baa43d6d617ddab2a459dd4931c69aa48
iclab <iclab@iclab-ui-Mac-Pro.local>
2017-04-10 20:40:32 +0900

    041020

--- lab-12-1-hello-rnn.py
+++ lab-12-1-hello-rnn.py
@@ -1,56 +1,60 @@
 # Lab 12 RNN
 import tensorflow as tf
 import numpy as np
-tf.set_random_seed(777)  # reproducibility
 
-idx2char = ['h', 'i', 'e', 'l', 'o']
-# Teach hello: hihell -> ihello
-x_data = [[0, 1, 0, 2, 3, 3]]   # hihell
-x_one_hot = [[[1, 0, 0, 0, 0],   # h 0
-              [0, 1, 0, 0, 0],   # i 1
-              [1, 0, 0, 0, 0],   # h 0
-              [0, 0, 1, 0, 0],   # e 2
-              [0, 0, 0, 1, 0],   # l 3
-              [0, 0, 0, 1, 0]]]  # l 3
+def run():
+    tf.set_random_seed(777)  # reproducibility
 
-y_data = [[1, 0, 2, 3, 3, 4]]    # ihello
+    idx2char = ['h', 'i', 'e', 'l', 'o']
+    # Teach hello: hihell -> ihello
+    x_data = [[0, 1, 0, 2, 3, 3]]   # hihell
+    x_one_hot = [[[1, 0, 0, 0, 0],   # h 0
+                  [0, 1, 0, 0, 0],   # i 1
+                  [1, 0, 0, 0, 0],   # h 0
+                  [0, 0, 1, 0, 0],   # e 2
+                  [0, 0, 0, 1, 0],   # l 3
+                  [0, 0, 0, 1, 0]]]  # l 3
 
-input_dim = 5  # one-hot size
-hidden_size = 5  # output from the LSTM. 5 to directly predict one-hot
-batch_size = 1   # one sentence
-sequence_length = 6  # |ihello| == 6
+    y_data = [[1, 0, 2, 3, 3, 4]]    # ihello
 
-X = tf.placeholder(
-    tf.float32, [None, sequence_length, hidden_size])  # X one-hot
-Y = tf.placeholder(tf.int32, [None, sequence_length])  # Y label
+    input_dim = 5  # one-hot size
+    hidden_size = 5  # output from the LSTM. 5 to directly predict one-hot
+    batch_size = 1   # one sentence
+    sequence_length = 6  # |ihello| == 6
 
-#cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
-cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
+    X = tf.placeholder(tf.float32, [None, sequence_length, hidden_size])  # X one-hot
+    Y = tf.placeholder(tf.int32, [None, sequence_length])  # Y label
 
-initial_state = cell.zero_state(batch_size, tf.float32)
-outputs, _states = tf.nn.dynamic_rnn(
-    cell, X, initial_state=initial_state, dtype=tf.float32)
+    #cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
+    cell = tf.contrib.rnn.core_rnn_cell.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
 
-weights = tf.ones([batch_size, sequence_length])
-#sequence_loss = tf.contrib.seq2seq.sequence_loss(logits=outputs, targets=Y, weights=weights)
-sequence_loss = tf.nn.seq2seq.sequence_loss_by_example(logits=outputs, targets=Y, weights=weights)
+    initial_state = cell.zero_state(batch_size, tf.float32)
+    outputs, _states = tf.nn.dynamic_rnn(cell, X, initial_state=initial_state, dtype=tf.float32)
+
+    weights = tf.ones([batch_size, sequence_length])
+    sequence_loss = tf.contrib.seq2seq.sequence_loss(logits=outputs, targets=Y, weights=weights)
+    #sequence_loss = tf.nn.seq2seq.sequence_loss_by_example(logits=outputs, targets=Y, weights=weights)
+
+    loss = tf.reduce_mean(sequence_loss)
+    train = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
+
+    prediction = tf.argmax(outputs, axis=2)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(2000):
+            l, _ = sess.run([loss, train], feed_dict={X: x_one_hot, Y: y_data})
+            result = sess.run(prediction, feed_dict={X: x_one_hot})
+            print(i, "loss:", l, "prediction: ", result, "true Y: ", y_data)
+
+            # print char using dic
+            result_str = [idx2char[c] for c in np.squeeze(result)]
+            print("\tPrediction str: ", ''.join(result_str))
 
 
-loss = tf.reduce_mean(sequence_loss)
-train = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
 
-prediction = tf.argmax(outputs, axis=2)
+run()
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(2000):
-        l, _ = sess.run([loss, train], feed_dict={X: x_one_hot, Y: y_data})
-        result = sess.run(prediction, feed_dict={X: x_one_hot})
-        print(i, "loss:", l, "prediction: ", result, "true Y: ", y_data)
-
-        # print char using dic
-        result_str = [idx2char[c] for c in np.squeeze(result)]
-        print("\tPrediction str: ", ''.join(result_str))
 
 '''
 0 loss: 1.55474 prediction:  [[3 3 3 3 4 4]] true Y:  [[1, 0, 2, 3, 3, 4]]
